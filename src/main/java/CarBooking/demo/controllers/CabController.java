043894aed488b883db167f90222229baaa42dd6b
Sahil Gupta <78966307+sahil-gupta01@users.noreply.github.com>
2024-02-04 14:18:09 +0530

    Update CabController.java

--- src/main/java/CarBooking/demo/controllers/CabController.java
+++ src/main/java/CarBooking/demo/controllers/CabController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
-//@RestController
+@RestController
 public class CabController {
 
     private CabManager cabManager;
